086c024ea0c38a204500db1a7cfebd10bcffd3d6
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-12-04 17:33:15 +0100

    some stuff :suspect:

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
+const app = express();
+
 
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
-let ejs = require('ejs');
-
-const app = express();
+const prisma = new PrismaClient();
+const bodypasrer = require('body-parser');
 
 app.use(express.json());
 
+app.set('view engine','ejs');
 
 app.get('/', async (req,res)=>{
     try {
-        const logon = await prisma.user.findMany();
-        res.json(logon);
-        res.sendFile(__dirname +'/index.html');
+        // const logon = await prisma.user.findMany();
+        // res.json(logon);
+       res.render('index',{Cool:"dkohajdlka"})
     } catch (error) {
         console.log(error);
 
