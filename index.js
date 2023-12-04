634b5779749ac6a320cf85443a4f54be76451131
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-12-04 12:49:50 +0100

    Installations

--- index.js
+++ index.js
@@ -3,9 +3,31 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
-    // ... you will write your Prisma Client queries here
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers)
+    await prisma.user.create({
+        data: {
+          name: 'Alce',
+          email: 'alice@prisa.io',
+          posts: {
+            create: { title: 'Hello Wrld' },
+          },
+          profile: {
+            create: { bio: 'I like turles' },
+          },
+        },
+      })
+    
+      const allUsers = await prisma.user.findMany({
+        include: {
+          posts: true,
+          profile: true,
+        },
+      })
+      console.dir(allUsers, { depth: null })
+      const post = await prisma.post.update({
+        where: { id: 1 },
+        data: { published: true },
+      })
+      console.log(post)
   }
 
 main()
@@ -17,25 +39,4 @@ main()
     await prisma.$disconnect()
     process.exit(1)
   })
-  async function main() {
-    await prisma.user.create({
-      data: {
-        name: 'Alice',
-        email: 'alice@prisma.io',
-        posts: {
-          create: { title: 'Hello World' },
-        },
-        profile: {
-          create: { bio: 'I like turtles' },
-        },
-      },
-    })
   
-    const allUsers = await prisma.user.findMany({
-      include: {
-        posts: true,
-        profile: true,
-      },
-    })
-    console.dir(allUsers, { depth: null })
-  }
