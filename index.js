d1f064a3acc38cf860e7bfcc10bf1f75d3237048
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-12-05 12:09:00 +0100

    DATABASE NOT WORKING REMEMBER TO FIX (to me)

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
+const { PrismaClient } = require('@prisma/client')
+
 const prisma = new PrismaClient()
 const express = require('express');
 
-const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 
 app.use(express.json());
-  
+
+
+async function main() {
+    await prisma.user.create({
+        data: {
+          name: 'Alce',
+          email: 'alice@prisa.io',
+          posts: {
+            create: { title: 'Hello Wrld' },
+          },
+          profile: {
+            create: { bio: 'I like turles' },
+          },
+        },
+      })
+    
+      const allUsers = await prisma.user.findMany({
+        include: {
+          posts: true,
+          profile: true,
+        },
+      })
+      console.dir(allUsers, { depth: null })
+      const post = await prisma.post.update({
+        where: { id: 1 },
+        data: { published: true },
+      })
+      console.log(post)
+  }
+
 main()
   .then(async () => {
     await prisma.$disconnect()
@@ -16,4 +46,5 @@ main()
     await prisma.$disconnect()
     process.exit(1)
   })
+  
   
